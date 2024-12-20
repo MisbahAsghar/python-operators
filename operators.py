3b7ed018bac3bb374b78c71023c07d52c64c5ae7
MisbahAsghar <asgharali675432@gmail.com>
2024-12-20 16:27:19 +0500

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,45 @@
+#airthematic operator
+print(6+4)    #addition
+print(10-5)   #subtract
+print(2*3)    #multiply
+print(15/3)   #division
+print(5**3)   #exponents
+print(13//2)  #floor division
+print(16%3)   #remainder/modulus
+
+num1: int = 6
+num2: int = 8
+
+sum: int = num1 + num2
+print(sum)
+sub: int = num1 - num2
+print(sub)
+div: int = num1 / num2
+print(div)
+mul: int = num1 * num2 
+print(mul)
+
+
+#assigment operators
+#to assign something
+x: int = 20
+x = x + 10
+print(x)
+
+#remainder/modulus
+x = x % 4
+print(x)
+
+#comparsion operator 
+#always give answer in True or False(boolean)
+#operands to compare krny k liye use hota ha
+
+num1: int = 15
+num2: int = 10
+
+print(num1 == num2)
+print(num1 != num2)
+print(num1 > num2)
+print(num1 < num2)
+print(num1 >= num2)
+print(num1 <= num2) 
